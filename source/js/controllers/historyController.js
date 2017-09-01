d0249c19458e7168cbe2f2b38892d1ff1f757786
Mordenius <mordenius@gmail.com>
2017-09-01 15:37:15 +0300

    chore(controllers/historyController) entry point

--- source/js/controllers/historyController.js
+++ source/js/controllers/historyController.js
@@ -1,15 +1,12 @@
 import replace from "lodash/replace";
 import words from "lodash/words";
+import size from "lodash/size";
+import includes from "lodash/includes";
 import { ControllerStateStore } from "redux-store-controller";
 
 class HistoryController extends ControllerStateStore {
 	constructor(options) {
-		options.stores.viewport.set({
-			viewport: HistoryController.ignoreLang(options.history.location.pathname)
-		});
-		options.stores.lang.set({
-			lang: words(options.history.location.pathname, /[^\/]+/g)[0] //eslint-disable-line
-		});
+		HistoryController.basic(options);
 		super(options);
 		this.history = options.history;
 	}
@@ -20,6 +17,41 @@ class HistoryController extends ControllerStateStore {
 
 	static ignoreLang(str) {
 		return replace(str, /\/ua|\/ru|\/en/, ``);
+	}
+
+	static basic(options){
+		const path = words(options.history.location.pathname, /[^\/]+/g); // eslint-disable-line
+
+		switch(size(path)){
+			case 0:
+			default:
+				break;
+			case 1:
+				if(includes(['ua', 'ru', 'en'], path[0])){
+					options.stores.lang.set({
+						lang: path[0]
+					});
+				}else{
+					options.stores.viewport.set({
+						viewport: `/${path[0]}`
+					});
+				}
+				break;
+			case 2:
+				if(includes(['ua', 'ru', 'en'], path[0])) {
+					options.stores.lang.set({
+						lang: path[0]
+					});
+					options.stores.viewport.set({
+						viewport: `/${path[1]}`
+					});
+				}else{
+					options.stores.viewport.set({
+						viewport: `/${path[0]}`
+					});
+				}
+				break;
+		}
 	}
 }
 
